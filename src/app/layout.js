6d83a70681f33f79e1f5354fb6a6a0f7eb6189ab
Michael <michael@uplord.co.uk>
2024-11-18 20:49:45 +0000

    remove font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,4 @@
-import { DM_Sans } from 'next/font/google'
 import "../styles/style.scss";
-
-const dmSans = DM_Sans({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
