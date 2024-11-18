91af78ec907c688e00521094308867e0e40add50
Michael <michael@uplord.co.uk>
2024-11-18 20:53:33 +0000

    remove font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,9 @@
 import Layout from "../components/layout";
-import { DM_Sans } from 'next/font/google'
 import "@/styles/style.scss";
-
-const dmSans = DM_Sans({ subsets: ['latin'] })
  
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout className={dmSans.className}>
+    <Layout>
       <Component {...pageProps} />
     </Layout>
   )
